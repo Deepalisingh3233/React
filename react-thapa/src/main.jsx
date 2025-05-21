0635166a6a169438f479da6297599a5c6e8e01d3
Deepali <deepalisingh3233@gmail.com>
2025-05-21 12:47:16 +0530

    Passing JSX as a prop

--- react-thapa/src/main.jsx
+++ react-thapa/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {App} from './App.jsx'
+import Profile from './components/Profile.jsx'
 // import { Practice } from './Practice.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {<App />}
+    {/* {<App />} */}
     {/* <Practice /> */}
+    <Profile/>
   </StrictMode>,
 )
